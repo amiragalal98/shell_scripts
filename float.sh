924adc86de8df12f86e17ad8683cc022d6e78d03
amiragalal98 <116448908+amiragalal98@users.noreply.github.com>
2023-03-10 21:49:58 +0200

    Update float.sh

--- float.sh
+++ float.sh
@@ -4,4 +4,12 @@ read num1
 echo "please enter second number"
 read num2
 sum=`echo $num1 + $num2 |bc`
-echo $sum
+echo the sum of two number is: $sum
+sub=`echo $num1 - $num2 |bc`
+echo the sub of two number is: $sub
+div=`echo $num1 / $num2 |bc`
+echo the div of two number is: $div
+multi=`echo $num1 \* $num2 |bc`
+echo the multi of two number is: $multi
+mod=`echo $num1 % $num2 |bc`
+echo the remain of division of two number is: $mod

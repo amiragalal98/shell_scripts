21ad5cf8074d44eb0015b7ecd489349f2b598f6b
amiragalal98 <amg18698@gmail.com>
2023-03-10 03:01:10 -0800

    script to read from user

---
+++ read.sh
@@ -0,0 +1,4 @@
+#this script demonstrates the usage of read command
+echo "please enter your name"
+read user_Name         #define variable user_name and store input in it
+echo "Hello, $user_Name, How may I help you ?"   # the dollar sign $ to access variable user_Name

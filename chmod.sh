f11c1a682632c3744596b67064ad1b0bbfc638ce
amiragalal98 <116448908+amiragalal98@users.noreply.github.com>
2023-03-10 15:12:40 +0200

    Update chmod.sh

--- chmod.sh
+++ chmod.sh
@@ -1,3 +1,3 @@
 #this file takes afile name and change mod permission as an argument
-chmod 744 $1
-ls -l $1
+chmod 744 $1      #to execute file write "sh chmod.sh filename"
+ls -l $1          #list long format for the file change  "-rwxr--r-- 1 root root 0 Mar 10 03:50 test"

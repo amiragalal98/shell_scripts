38b59a2f4541e504d774d1f19c3020ed9e64f1c7
amiragalal98 <amg18698@gmail.com>
2023-03-10 02:37:02 -0800

    some command

---
+++ shell2.sh
@@ -0,0 +1,4 @@
+#this is the second  script this pwd ,ls command
+pwd #to find path of your current working direcory
+ls -la #list file and directories and hidden in long format
+echo "to execute the file write sh filename"

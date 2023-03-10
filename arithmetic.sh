1386c31f201bbeb8e61dbe681ddd83dee24317a3
amiragalal98 <116448908+amiragalal98@users.noreply.github.com>
2023-03-10 19:41:47 +0200

    Update arithmetic.sh

--- arithmetic.sh
+++ arithmetic.sh
@@ -6,5 +6,5 @@ read num2
 echo the sum of two number is `expr $num1 + $num2`
 echo the substract of two number is `expr $num1 - $num2`
 echo the multiple of two number is `expr $num1 \* $num2`
-echo th division of two number is `expr $num1 / $num2`
+echo th division of two number is  `expr $num1 / $num2`    #in integer
 echo the remain of division is % `expr $num1 % $num2`

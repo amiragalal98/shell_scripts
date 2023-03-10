35227cee4091cfd9383065f74bf1f84d3bfd3810
amiragalal98 <116448908+amiragalal98@users.noreply.github.com>
2023-03-10 16:26:23 +0200

    Update whoami.sh

--- whoami.sh
+++ whoami.sh
@@ -1,6 +1,6 @@
 #rename a file to name.whoami
 #where name is the login name of the user executing the script
-name=$1
-set `who am i`
-mv $name $name.$1
+name=$1      # $1=file name you enter =name =argument of name file
+set `who am i`      #now $1=who am i value
+mv $name $name.$1   #rename to namefile.whoami
 ls

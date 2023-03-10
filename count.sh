915b0f04da091a473dc1f2a8f8d7c35268dc7f8f
amiragalal98 <116448908+amiragalal98@users.noreply.github.com>
2023-03-10 18:56:54 +0200

    Update count.sh

--- count.sh
+++ count.sh
@@ -1,4 +1,4 @@
-#count the number of command using  $#
+#Count The Number of Command Line Arguments Using $#
 set the youtube is very useful to learn
 echo "the count number of cammand is $#"
 echo $#

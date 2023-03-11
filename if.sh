b5a5b6b45afc925580495c995fe22c77b0d659b0
amiragalal98 <amg18698@gmail.com>
2023-03-11 03:09:13 -0800

    if then statement

--- if.sh
+++ if.sh
@@ -4,6 +4,6 @@ echo " please enter source and target file names"
 read source target
 if mv $source $target
 then
-echo "you file has been successfully renamae"
+echo "you file has been successfully rename"
 ls
 fi    #end id 

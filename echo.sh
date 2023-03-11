2893681aab0832af4020e7b89f1cac5bfc95d6c9
amiragalal98 <amg18698@gmail.com>
2023-03-11 00:26:33 -0800

    echo command

---
+++ echo.sh
@@ -0,0 +1,11 @@
+#echo Escape sequence
+echo  "hello world"
+echo "you can start new line using \n hi from new line"
+echo "i will not display cause \r now we escape some words"
+echo "we can't be near cause \t we are far"
+echo " let try what happen\b when we write this"         #/b will delete n character in happen
+echo "\033[1mthis text display in bold 1m 0m \033[0m"
+#echo "\033[1mthis text display in bold and all follow this statment 1m"
+echo "\033[2mchange format color gray 2m\033[0m"
+echo "\033[4mchange format underline 4m\033[0m"
+echo "\033[7mchange format hightlight 7m\033[0m"
